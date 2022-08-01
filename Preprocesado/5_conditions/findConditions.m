71a9303564479424c8b19b3a2806c87d56aa26a3
ChemaGP-UGR <64792222+ChemaGP-UGR@users.noreply.github.com>
2022-08-01 20:57:40 +0200

    Delete Preprocesado directory

--- Preprocesado/5_conditions/findConditions.m
+++
@@ -1,8 +0,0 @@
-function res = findConditions(first, last, target, events_to_extract)
-res = {};
-    for i = first : last
-        if startsWith(events_to_extract{i},target)
-            res = [res; events_to_extract{i}];
-        end
-    end
-end
